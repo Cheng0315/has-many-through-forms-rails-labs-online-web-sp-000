7046558a6da91b433992f316a86b520b44a7d9c9
Cheng Vue <csalvue@gmail.com>
2019-01-31 16:01:41 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,5 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user
 
-  def user_id=(user_id)
-    @user = User.find(user_id)
-    @user.comments << self
-  end
+
 end
