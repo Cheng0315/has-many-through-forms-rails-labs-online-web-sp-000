95329e9d2bdb777dd6d3bb33f84692034cd608be
Cheng Vue <csalvue@gmail.com>
2019-01-31 16:06:29 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,5 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories, reject_if: :all_blank
 
 
-  def comments_attributes(comment_attributes)
-    comment_attributes.values.each do |comment_attribute|
-      comment = Comment.find_or_create_by(comment_attribute)
-      self.comments.build(comment: comment)
-    end
-  end
+  
 end
